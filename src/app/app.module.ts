f0fcad2f16602116b23607ee66953d8473ffe88a
Gokul Raj <gokulnanshirk@gmail.com>
2019-12-26 20:56:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { AdminComponent } from "./admin/admin.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ListComponent } from './list/list.component';
+
+const appRoutes: Routes = [
+  { path: "", component: ListComponent },
+  { path: "admin", component: AdminComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AdminComponent, ListComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class DataService {
+  data: any;
+  constructor() {
+    this.data = ["abcd", "efgh"];
+  }
+}
