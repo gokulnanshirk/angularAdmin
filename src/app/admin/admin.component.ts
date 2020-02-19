d66d9dd9329665c12c6fe6aeaef004e6ebfe794c
Gokul Raj <gokulnanshirk@gmail.com>
2020-02-19 22:34:56 +0530

    ddl

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,19 +7,32 @@ import { DataService } from "../data.service";
   styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
-  constructor(private service: DataService) {}
-  name: any;
-  age: any;
-  place: any;
-  ngOnInit() {}
+  constructor(private service: DataService) { }
+  // name: any;
+  // age: any;
+  // place: any;
+  students: any
+  currentDepartment: any
+  options: any
+  ngOnInit() {
+    this.currentDepartment = 'CIVIL'
+    this.students = [{ name: 'dinesh', dept: 'CIVIL' }, { name: 'gokul', dept: 'MECH' },{ name: 'mohan', dept: 'CIVIL' }, { name: 'nikhil', dept: 'MECH' }]
+    this.change()
+  }
 
   submit() {
-    console.log(this.name, this.age, this.place);
-    let obj = {
-      name: this.name,
-      age: this.age,
-      place: this.place
-    };
-    this.service.set(obj)
+    // console.log(this.name, this.age, this.place);
+    // let obj = {
+    //   name: this.name,
+    //   age: this.age,
+    //   place: this.place
+    // };
+    // this.service.set(obj)
+  }
+  change() {
+    this.options = this.students.filter(item => {
+      return item.dept != this.currentDepartment
+    })
+    console.log(this.currentDepartment,this.options, 'changed----')
   }
 }
